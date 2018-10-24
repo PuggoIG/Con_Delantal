13e436d208fccafbdc0d4eaaf8c4cedd6e53b22d
Kelebra <ricardokelebra@gmail.com>
2018-10-24 12:32:50 -0500

    Actualizar rutas
    Signed-off-by: Kelebra <ricardokelebra@gmail.com>

---
+++ server/routes/index.js
@@ -0,0 +1,28 @@
+const  express =  require("express");
+const router = express.Router();
+const isAuthenticated =  require("../middlewares/isAuthenticated");
+const {calculatePriceValidation} = require("../middlewares/validations")
+const {signUp,logIn,me} =  require("../controllers/users");
+const {createRestuarant,getAllRestaurants,getOneRestuarant} =  require("../controllers/houses");
+const {calculatePrice,createOder} = require('../controllers/orders');
+
+
+
+router.post('/users/signup',signUp)
+router.post('/users/login',logIn)
+router.get('/users/me',isAuthenticated,me)
+
+
+router.post('/restuarants',isAuthenticated,createRestuarant)
+router.get('/restuarants',getAllRestaurants)
+router.get('/restuarants/:id',getOneRestuarant)
+
+
+router.post('/orders/calculate',calculatePriceValidation,calculatePrice);
+router.post('/orders',isAuthenticated,createOder);
+
+
+
+
+module.exports = router;
+
