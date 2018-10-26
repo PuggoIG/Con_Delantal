67eb97450364b0062a12af4f2359605207f443e6
Kelebra <ricardokelebra@gmail.com>
2018-10-25 23:39:15 -0500

    cambie el puerto 1000
    Signed-off-by: Kelebra <ricardokelebra@gmail.com>

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const routes =  require('./routes/index');
 
 const app =  express();
 
-const port  = process.env.PORT || 10000 
+const port  = process.env.PORT || 5000 
 
 const {errors} = require("celebrate");
 
